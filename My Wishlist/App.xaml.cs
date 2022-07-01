333c03f5a33d3f2b2218597eed9f70bdae464e58
rpitogo@fullscale.io <rpitogo@fullscale.io>
2022-07-01 15:52:40 +0800

    Add project files.

---
+++ My Wishlist/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace My_Wishlist;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ My Wishlist/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace My_Wishlist;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
